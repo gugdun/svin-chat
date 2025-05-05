410386b97dfe509b753284cb218264c5867707fe
gugdun <gugdunoff@gmail.com>
2025-05-06 01:24:58 +0300

    add: Basic structure

--- src/index.js
+++ src/index.js
@@ -2,22 +2,36 @@
 // All rights reserved. Unauthorized use, copying, or distribution is strictly prohibited.
 
 var express = require("express");
-var app = express();
-var longpoll = require("express-longpoll")(app);
-
+var ejs = require("ejs");
 require("dotenv").config();
+
 const PORT = process.env.PORT || 5000;
 
+var app = express();
+app.set("view engine", "ejs");
+app.use(express.static("public"));
+
+var longpoll = require("express-longpoll")(app);
 longpoll.create("/poll");
+
+app.get("/", async (req, res) => {
+    res.render("layout", { child: await ejs.renderFile("views/home.ejs") });
+});
+
+app.get("/login", async (req, res) => {
+    res.render("layout", { child: await ejs.renderFile("views/login.ejs") });
+});
+
+app.get("/register", async (req, res) => {
+    res.render("layout", { child: await ejs.renderFile("views/register.ejs") });
+});
 
 app.listen(PORT, function() {
     console.log(`Listening on port ${PORT}`);
 });
 
 var data = { message: "Test" };
-
 longpoll.publish("/poll", data);
-
 setInterval(function () { 
     longpoll.publish("/poll", data);
 }, 5000);
